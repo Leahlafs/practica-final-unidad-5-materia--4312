734f33ef45c9694cc034124e1dfac1b3f920485d
Leahlafs <leydianafrias83@gmail.com>
2025-11-21 19:12:13 -0400

    funciones formulario pais

--- ViajArt/FrmPais.Designer.cs
+++ ViajArt/FrmPais.Designer.cs
@@ -28,12 +28,188 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.btnAgregar = new System.Windows.Forms.Button();
+            this.btnActualizar = new System.Windows.Forms.Button();
+            this.txtNombre = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnEliminar = new System.Windows.Forms.Button();
+            this.txtIDpais = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnMostrar = new System.Windows.Forms.Button();
+            this.dgvPais = new System.Windows.Forms.DataGridView();
+            this.txtNombreActualizar = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtIdActualizar = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvPais)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.txtIdActualizar);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.txtNombreActualizar);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.btnAgregar);
+            this.groupBox1.Controls.Add(this.btnActualizar);
+            this.groupBox1.Controls.Add(this.txtNombre);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.btnEliminar);
+            this.groupBox1.Controls.Add(this.txtIDpais);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Location = new System.Drawing.Point(156, 41);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(378, 215);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "groupBox1";
+            // 
+            // btnAgregar
+            // 
+            this.btnAgregar.Location = new System.Drawing.Point(25, 175);
+            this.btnAgregar.Name = "btnAgregar";
+            this.btnAgregar.Size = new System.Drawing.Size(75, 23);
+            this.btnAgregar.TabIndex = 6;
+            this.btnAgregar.Text = "Agregar";
+            this.btnAgregar.UseVisualStyleBackColor = true;
+            this.btnAgregar.Click += new System.EventHandler(this.btnAgregar_Click);
+            // 
+            // btnActualizar
+            // 
+            this.btnActualizar.Location = new System.Drawing.Point(277, 175);
+            this.btnActualizar.Name = "btnActualizar";
+            this.btnActualizar.Size = new System.Drawing.Size(75, 23);
+            this.btnActualizar.TabIndex = 5;
+            this.btnActualizar.Text = "Actualizar";
+            this.btnActualizar.UseVisualStyleBackColor = true;
+            this.btnActualizar.Click += new System.EventHandler(this.btnActualizar_Click);
+            // 
+            // txtNombre
+            // 
+            this.txtNombre.Location = new System.Drawing.Point(25, 76);
+            this.txtNombre.Name = "txtNombre";
+            this.txtNombre.Size = new System.Drawing.Size(100, 20);
+            this.txtNombre.TabIndex = 4;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(22, 60);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(83, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Nombre del pais";
+            // 
+            // btnEliminar
+            // 
+            this.btnEliminar.Location = new System.Drawing.Point(141, 175);
+            this.btnEliminar.Name = "btnEliminar";
+            this.btnEliminar.Size = new System.Drawing.Size(75, 23);
+            this.btnEliminar.TabIndex = 2;
+            this.btnEliminar.Text = "Eliminar";
+            this.btnEliminar.UseVisualStyleBackColor = true;
+            this.btnEliminar.Click += new System.EventHandler(this.btnEliminar_Click);
+            // 
+            // txtIDpais
+            // 
+            this.txtIDpais.Location = new System.Drawing.Point(141, 76);
+            this.txtIDpais.Name = "txtIDpais";
+            this.txtIDpais.Size = new System.Drawing.Size(100, 20);
+            this.txtIDpais.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(138, 60);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(41, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "ID Pais";
+            // 
+            // btnMostrar
+            // 
+            this.btnMostrar.Location = new System.Drawing.Point(158, 262);
+            this.btnMostrar.Name = "btnMostrar";
+            this.btnMostrar.Size = new System.Drawing.Size(286, 23);
+            this.btnMostrar.TabIndex = 7;
+            this.btnMostrar.Text = "Mostrar";
+            this.btnMostrar.UseVisualStyleBackColor = true;
+            // 
+            // dgvPais
+            // 
+            this.dgvPais.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvPais.Location = new System.Drawing.Point(156, 318);
+            this.dgvPais.Name = "dgvPais";
+            this.dgvPais.Size = new System.Drawing.Size(288, 167);
+            this.dgvPais.TabIndex = 8;
+            // 
+            // txtNombreActualizar
+            // 
+            this.txtNombreActualizar.Location = new System.Drawing.Point(252, 92);
+            this.txtNombreActualizar.Name = "txtNombreActualizar";
+            this.txtNombreActualizar.Size = new System.Drawing.Size(100, 20);
+            this.txtNombreActualizar.TabIndex = 8;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(249, 76);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(83, 13);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Nombre del pais";
+            // 
+            // txtIdActualizar
+            // 
+            this.txtIdActualizar.Location = new System.Drawing.Point(252, 44);
+            this.txtIdActualizar.Name = "txtIdActualizar";
+            this.txtIdActualizar.Size = new System.Drawing.Size(100, 20);
+            this.txtIdActualizar.TabIndex = 10;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(249, 28);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(41, 13);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "ID Pais";
+            // 
+            // FrmPais
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(664, 530);
+            this.Controls.Add(this.dgvPais);
+            this.Controls.Add(this.btnMostrar);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "FrmPais";
             this.Text = "FrmPais";
+            this.Load += new System.EventHandler(this.FrmPais_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvPais)).EndInit();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TextBox txtNombre;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnEliminar;
+        private System.Windows.Forms.TextBox txtIDpais;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnAgregar;
+        private System.Windows.Forms.Button btnActualizar;
+        private System.Windows.Forms.Button btnMostrar;
+        private System.Windows.Forms.DataGridView dgvPais;
+        private System.Windows.Forms.TextBox txtNombreActualizar;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtIdActualizar;
+        private System.Windows.Forms.Label label4;
     }
 }
--- ViajArt/FrmPais.cs
+++ ViajArt/FrmPais.cs
@@ -8,13 +8,135 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
+
 namespace ViajArt
 {
     public partial class FrmPais : Form
     {
+        AgenciaToursDBEntities db = new AgenciaToursDBEntities();
+
         public FrmPais()
         {
             InitializeComponent();
         }
+
+
+
+        private void FrmPais_Load(object sender, EventArgs e)
+        {
+            CargarDatos();
+        }
+
+        private void CargarDatos()
+        {
+            db = new AgenciaToursDBEntities(); // refrescar contexto
+            dgvPais.DataSource = db.Pais.ToList();
+
+            dgvPais.Columns["Destinos"].Visible = false;
+            dgvPais.Columns["Tours"].Visible = false;
+
+
+        }
+
+
+        private void btnAgregar_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (string.IsNullOrWhiteSpace(txtNombre.Text))
+                {
+                    MessageBox.Show("Debe escribir un nombre.");
+                    return;
+                }
+
+                Pais p = new Pais();
+                p.Nombre = txtNombre.Text;
+
+                db.Pais.Add(p);
+                db.SaveChanges();
+
+                MessageBox.Show("País agregado correctamente.");
+                CargarDatos();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error al agregar: " + ex.Message);
+            }
+        }
+
+
+
+        private void btnActualizar_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (string.IsNullOrWhiteSpace(txtIdActualizar.Text))
+                {
+                    MessageBox.Show("Debe escribir un ID para actualizar.");
+                    return;
+                }
+
+                int id = int.Parse(txtIdActualizar.Text);
+                var pais = db.Pais.FirstOrDefault(x => x.PaisID == id);
+
+                if (pais != null)
+                {
+                    if (string.IsNullOrWhiteSpace(txtNombreActualizar.Text))
+                    {
+                        MessageBox.Show("Debe escribir un nuevo nombre.");
+                        return;
+                    }
+
+                    pais.Nombre = txtNombreActualizar.Text;
+                    db.SaveChanges();
+
+                    MessageBox.Show("País actualizado correctamente.");
+                    CargarDatos();
+                }
+                else
+                {
+                    MessageBox.Show("El ID no existe.");
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error al actualizar: " + ex.Message);
+            }
+
+        }
+
+        private void btnEliminar_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (txtIDpais.Text == "")
+                {
+                    MessageBox.Show("Debe escribir un ID para eliminar.");
+                    return;
+                }
+
+                int id = int.Parse(txtIDpais.Text);
+                var pais = db.Pais.FirstOrDefault(x => x.PaisID == id);
+
+                if (pais != null)
+                {
+                    db.Pais.Remove(pais);
+                    db.SaveChanges();
+
+                    MessageBox.Show("País eliminado correctamente.");
+                    CargarDatos();
+                  
+                }
+                else
+                {
+                    MessageBox.Show("El ID no existe.");
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error al eliminar: " + ex.Message);
+            }
+        }
     }
 }
+    
