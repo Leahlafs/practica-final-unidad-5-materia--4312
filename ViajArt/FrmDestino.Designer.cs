ac00ad15fc963621950a29df9365da4f4b6a8d06
Leahlafs <leydianafrias83@gmail.com>
2025-11-23 02:37:14 -0400

    funciones para destino

--- ViajArt/FrmDestino.Designer.cs
+++ ViajArt/FrmDestino.Designer.cs
@@ -28,12 +28,337 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.dgvDestino = new System.Windows.Forms.DataGridView();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtNombre = new System.Windows.Forms.TextBox();
+            this.cmbPais = new System.Windows.Forms.ComboBox();
+            this.btnAgregar = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtIdActualizar = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.btnActualizar = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.cmbPaisActualizar = new System.Windows.Forms.ComboBox();
+            this.txtNombreActualizar = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txtIdEliminar = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.btnEliminar = new System.Windows.Forms.Button();
+            this.nudDias = new System.Windows.Forms.NumericUpDown();
+            this.nudDiasActualizar = new System.Windows.Forms.NumericUpDown();
+            this.nudHoras = new System.Windows.Forms.NumericUpDown();
+            this.nudHorasActualizar = new System.Windows.Forms.NumericUpDown();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.btnMostrar = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvDestino)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.nudDias)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nudDiasActualizar)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nudHoras)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nudHorasActualizar)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dgvDestino
+            // 
+            this.dgvDestino.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvDestino.Location = new System.Drawing.Point(37, 475);
+            this.dgvDestino.Name = "dgvDestino";
+            this.dgvDestino.RowTemplate.Height = 28;
+            this.dgvDestino.Size = new System.Drawing.Size(998, 239);
+            this.dgvDestino.TabIndex = 0;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.label8);
+            this.groupBox1.Controls.Add(this.label9);
+            this.groupBox1.Controls.Add(this.label10);
+            this.groupBox1.Controls.Add(this.label7);
+            this.groupBox1.Controls.Add(this.nudHorasActualizar);
+            this.groupBox1.Controls.Add(this.nudHoras);
+            this.groupBox1.Controls.Add(this.nudDiasActualizar);
+            this.groupBox1.Controls.Add(this.nudDias);
+            this.groupBox1.Controls.Add(this.btnEliminar);
+            this.groupBox1.Controls.Add(this.txtIdEliminar);
+            this.groupBox1.Controls.Add(this.label6);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.cmbPaisActualizar);
+            this.groupBox1.Controls.Add(this.txtNombreActualizar);
+            this.groupBox1.Controls.Add(this.label5);
+            this.groupBox1.Controls.Add(this.btnActualizar);
+            this.groupBox1.Controls.Add(this.txtIdActualizar);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.btnAgregar);
+            this.groupBox1.Controls.Add(this.cmbPais);
+            this.groupBox1.Controls.Add(this.txtNombre);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Location = new System.Drawing.Point(37, 36);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(998, 359);
+            this.groupBox1.TabIndex = 1;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "groupBox1";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(33, 53);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(146, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Nombre del destino";
+            // 
+            // txtNombre
+            // 
+            this.txtNombre.Location = new System.Drawing.Point(185, 53);
+            this.txtNombre.Name = "txtNombre";
+            this.txtNombre.Size = new System.Drawing.Size(100, 26);
+            this.txtNombre.TabIndex = 1;
+            // 
+            // cmbPais
+            // 
+            this.cmbPais.FormattingEnabled = true;
+            this.cmbPais.Location = new System.Drawing.Point(164, 85);
+            this.cmbPais.Name = "cmbPais";
+            this.cmbPais.Size = new System.Drawing.Size(121, 28);
+            this.cmbPais.TabIndex = 2;
+            // 
+            // btnAgregar
+            // 
+            this.btnAgregar.AutoEllipsis = true;
+            this.btnAgregar.Location = new System.Drawing.Point(164, 250);
+            this.btnAgregar.Name = "btnAgregar";
+            this.btnAgregar.Size = new System.Drawing.Size(101, 31);
+            this.btnAgregar.TabIndex = 3;
+            this.btnAgregar.Text = "Agregar";
+            this.btnAgregar.UseVisualStyleBackColor = true;
+            this.btnAgregar.Click += new System.EventHandler(this.btnAgregar_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(107, 85);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(39, 20);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Pais";
+            // 
+            // txtIdActualizar
+            // 
+            this.txtIdActualizar.Location = new System.Drawing.Point(813, 31);
+            this.txtIdActualizar.Name = "txtIdActualizar";
+            this.txtIdActualizar.Size = new System.Drawing.Size(100, 26);
+            this.txtIdActualizar.TabIndex = 6;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(697, 31);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(107, 20);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "ID del destino";
+            // 
+            // btnActualizar
+            // 
+            this.btnActualizar.AutoEllipsis = true;
+            this.btnActualizar.Location = new System.Drawing.Point(833, 311);
+            this.btnActualizar.Name = "btnActualizar";
+            this.btnActualizar.Size = new System.Drawing.Size(101, 31);
+            this.btnActualizar.TabIndex = 7;
+            this.btnActualizar.Text = "Actualizar";
+            this.btnActualizar.UseVisualStyleBackColor = true;
+            this.btnActualizar.Click += new System.EventHandler(this.btnActualizar_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(719, 119);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(88, 20);
+            this.label4.TabIndex = 11;
+            this.label4.Text = "Nuevo Pais";
+            this.label4.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // cmbPaisActualizar
+            // 
+            this.cmbPaisActualizar.FormattingEnabled = true;
+            this.cmbPaisActualizar.Location = new System.Drawing.Point(813, 116);
+            this.cmbPaisActualizar.Name = "cmbPaisActualizar";
+            this.cmbPaisActualizar.Size = new System.Drawing.Size(121, 28);
+            this.cmbPaisActualizar.TabIndex = 10;
+            // 
+            // txtNombreActualizar
+            // 
+            this.txtNombreActualizar.Location = new System.Drawing.Point(811, 68);
+            this.txtNombreActualizar.Name = "txtNombreActualizar";
+            this.txtNombreActualizar.Size = new System.Drawing.Size(100, 26);
+            this.txtNombreActualizar.TabIndex = 9;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(695, 68);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(112, 20);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "Nuevo nombre";
+            // 
+            // txtIdEliminar
+            // 
+            this.txtIdEliminar.Location = new System.Drawing.Point(515, 78);
+            this.txtIdEliminar.Name = "txtIdEliminar";
+            this.txtIdEliminar.Size = new System.Drawing.Size(100, 26);
+            this.txtIdEliminar.TabIndex = 13;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(402, 78);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(107, 20);
+            this.label6.TabIndex = 12;
+            this.label6.Text = "ID del destino";
+            // 
+            // btnEliminar
+            // 
+            this.btnEliminar.AutoEllipsis = true;
+            this.btnEliminar.Location = new System.Drawing.Point(457, 126);
+            this.btnEliminar.Name = "btnEliminar";
+            this.btnEliminar.Size = new System.Drawing.Size(101, 31);
+            this.btnEliminar.TabIndex = 14;
+            this.btnEliminar.Text = "Eliminar";
+            this.btnEliminar.UseVisualStyleBackColor = true;
+            this.btnEliminar.Click += new System.EventHandler(this.btnEliminar_Click);
+            // 
+            // nudDias
+            // 
+            this.nudDias.Location = new System.Drawing.Point(165, 120);
+            this.nudDias.Name = "nudDias";
+            this.nudDias.Size = new System.Drawing.Size(120, 26);
+            this.nudDias.TabIndex = 15;
+            // 
+            // nudDiasActualizar
+            // 
+            this.nudDiasActualizar.Location = new System.Drawing.Point(814, 158);
+            this.nudDiasActualizar.Name = "nudDiasActualizar";
+            this.nudDiasActualizar.Size = new System.Drawing.Size(120, 26);
+            this.nudDiasActualizar.TabIndex = 16;
+            // 
+            // nudHoras
+            // 
+            this.nudHoras.Location = new System.Drawing.Point(165, 152);
+            this.nudHoras.Name = "nudHoras";
+            this.nudHoras.Size = new System.Drawing.Size(120, 26);
+            this.nudHoras.TabIndex = 17;
+            // 
+            // nudHorasActualizar
+            // 
+            this.nudHorasActualizar.Location = new System.Drawing.Point(811, 190);
+            this.nudHorasActualizar.Name = "nudHorasActualizar";
+            this.nudHorasActualizar.Size = new System.Drawing.Size(120, 26);
+            this.nudHorasActualizar.TabIndex = 18;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(20, 126);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(128, 20);
+            this.label7.TabIndex = 19;
+            this.label7.Text = "Duración en días";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(20, 158);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(139, 20);
+            this.label10.TabIndex = 22;
+            this.label10.Text = "Duración en horas";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(666, 192);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(139, 20);
+            this.label8.TabIndex = 24;
+            this.label8.Text = "Duración en horas";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(666, 160);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(128, 20);
+            this.label9.TabIndex = 23;
+            this.label9.Text = "Duración en días";
+            // 
+            // btnMostrar
+            // 
+            this.btnMostrar.AutoEllipsis = true;
+            this.btnMostrar.Location = new System.Drawing.Point(340, 420);
+            this.btnMostrar.Name = "btnMostrar";
+            this.btnMostrar.Size = new System.Drawing.Size(312, 38);
+            this.btnMostrar.TabIndex = 25;
+            this.btnMostrar.Text = "Mostar";
+            this.btnMostrar.UseVisualStyleBackColor = true;
+            this.btnMostrar.Click += new System.EventHandler(this.btnMostrar_Click);
+            // 
+            // FrmDestino
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1069, 772);
+            this.Controls.Add(this.btnMostrar);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.dgvDestino);
+            this.Name = "FrmDestino";
             this.Text = "FrmDestino";
+            this.Load += new System.EventHandler(this.FrmDestino_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvDestino)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.nudDias)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nudDiasActualizar)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nudHoras)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nudHorasActualizar)).EndInit();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.DataGridView dgvDestino;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnAgregar;
+        private System.Windows.Forms.ComboBox cmbPais;
+        private System.Windows.Forms.TextBox txtNombre;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.ComboBox cmbPaisActualizar;
+        private System.Windows.Forms.TextBox txtNombreActualizar;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button btnActualizar;
+        private System.Windows.Forms.TextBox txtIdActualizar;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button btnEliminar;
+        private System.Windows.Forms.TextBox txtIdEliminar;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.NumericUpDown nudHorasActualizar;
+        private System.Windows.Forms.NumericUpDown nudHoras;
+        private System.Windows.Forms.NumericUpDown nudDiasActualizar;
+        private System.Windows.Forms.NumericUpDown nudDias;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Button btnMostrar;
     }
 }
--- ViajArt/FrmDestino.cs
+++ ViajArt/FrmDestino.cs
@@ -12,9 +12,156 @@ namespace ViajArt
 {
     public partial class FrmDestino : Form
     {
+
+        AgenciaToursDBEntities db = new AgenciaToursDBEntities();
+
         public FrmDestino()
         {
             InitializeComponent();
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void FrmDestino_Load(object sender, EventArgs e)
+        {
+            CargarDatos();
+            CargarPaises();
+
+        }
+
+        private void CargarDatos()
+        {
+            var destinos = db.Destinos
+                .Select(d => new
+                {
+                    d.DestinoID,
+                    d.Nombre,
+                    Pais = d.Pais.Nombre,
+                    d.DuracionDias,
+                    d.DuracionHoras
+                })
+                .ToList();
+
+            dgvDestino.DataSource = destinos;
+        }
+
+        private void CargarPaises()
+        {
+            var paises = db.Pais.ToList();
+            cmbPais.DataSource = paises;
+            cmbPais.DisplayMember = "Nombre";
+            cmbPais.ValueMember = "PaisID";
+
+            cmbPaisActualizar.DataSource = paises;
+            cmbPaisActualizar.DisplayMember = "Nombre";
+            cmbPaisActualizar.ValueMember = "PaisID";
+        }
+
+        private void btnAgregar_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (string.IsNullOrWhiteSpace(txtNombre.Text))
+                {
+                    MessageBox.Show("Debe escribir un nombre.");
+                    return;
+                }
+
+                Destinos d = new Destinos();
+                d.Nombre = txtNombre.Text;
+                d.PaisID = (int)cmbPais.SelectedValue;
+                d.DuracionDias = (int)nudDias.Value;
+                d.DuracionHoras = (int)nudHoras.Value;
+
+                db.Destinos.Add(d);
+                db.SaveChanges();
+
+                MessageBox.Show("Destino agregado correctamente.");
+                CargarDatos();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error al agregar: " + ex.Message);
+            }
+
+        }
+
+        private void btnActualizar_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (string.IsNullOrWhiteSpace(txtIdActualizar.Text))
+                {
+                    MessageBox.Show("Debe escribir un ID para actualizar.");
+                    return;
+                }
+
+                int id = int.Parse(txtIdActualizar.Text);
+                var destino = db.Destinos.FirstOrDefault(x => x.DestinoID == id);
+
+                if (destino != null)
+                {
+                    destino.Nombre = txtNombreActualizar.Text;
+                    destino.PaisID = (int)cmbPaisActualizar.SelectedValue;
+                    destino.DuracionDias = (int)nudDiasActualizar.Value;
+                    destino.DuracionHoras = (int)nudHorasActualizar.Value;
+
+                    db.SaveChanges();
+
+                    MessageBox.Show("Destino actualizado correctamente.");
+                    CargarDatos();
+                }
+                else
+                {
+                    MessageBox.Show("El ID no existe.");
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error al actualizar: " + ex.Message);
+            }
+
+        }
+
+        private void btnEliminar_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (string.IsNullOrWhiteSpace(txtIdEliminar.Text))
+                {
+                    MessageBox.Show("Debe escribir un ID para eliminar.");
+                    return;
+                }
+
+                int id = int.Parse(txtIdEliminar.Text);
+                var destino = db.Destinos.FirstOrDefault(x => x.DestinoID == id);
+
+                if (destino != null)
+                {
+                    db.Destinos.Remove(destino);
+                    db.SaveChanges();
+
+                    MessageBox.Show("Destino eliminado correctamente.");
+                    CargarDatos();
+                }
+                else
+                {
+                    MessageBox.Show("El ID no existe.");
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error al eliminar: " + ex.Message);
+            }
+
+        }
+
+        private void btnMostrar_Click(object sender, EventArgs e)
+        {
+            CargarDatos();
         }
     }
 }
